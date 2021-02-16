7a5df669f6dc2035c1df1f7ed06b9cb15867950f
tomorisakura <resky67@gmail.com>
2021-02-16 12:31:51 +0700

    refactor code

--- src/middleware/auth.mjs
+++ src/middleware/auth.mjs
@@ -14,10 +14,9 @@ export default class Auth {
                 jwt.verify(bearerToken, process.env.ACCESS_TOKEN_KEY, (err, decoded) => {
                     if(err) {
                         res.status(401).send({ message: 'token expired' });
-                        res.end();
                         throw err;
                     }
-                    req.token = decoded;
+                    req.data = decoded;
                     next();
                 });
             } else {
